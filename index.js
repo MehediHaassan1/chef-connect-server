8c5a33f6b19807ba2c4ea7ef51c1ba6c0c2bbaa6
Mehedi Hassan Barat <mehedi.haassan1@gmail.com>
2024-01-19 15:16:54 +0600

    Indivisual chef details api done

--- index.js
+++ index.js
@@ -4,10 +4,29 @@ const port = 5000
 const cors = require('cors')
 app.use(cors())
 
+
+// ------ Data ------ //
+
+const chefs = require('./data/chefs.json')
+
+
+// ------ Data ------ //
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+    res.send('Hello World!')
+});
+
+app.get('/chefs', (req, res) => {
+    res.send(chefs);
+});
+
+app.get('/chef/:chefId', (req, res) => {
+    const chefId = req.params.chefId;
+    const chefDetails = chefs.find(chef => chef._id === chefId)
+    res.send(chefDetails);
+    console.log(chefDetails);
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+    console.log(`Example app listening on port ${port}`)
+});
