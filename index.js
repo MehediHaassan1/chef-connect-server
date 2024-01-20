18d410cdcc3ae635b5929a34e222be2c2a3bd6f7
Mehedi Hassan Barat <mehedi.haassan1@gmail.com>
2024-01-20 19:27:42 +0600

    modified for cors problem

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
-const express = require('express')
+const express = require("express")
+const cors = require("cors")
 const app = express()
-const port = 5000
-const cors = require('cors')
-app.use(cors())
+const port = process.env.PORT || 5000
+// app.use(cors())
+
+app.use(
+    cors({
+        origin: "*",
+    })
+);
 
 
 // ------ Data ------ //
